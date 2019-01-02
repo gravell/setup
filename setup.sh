04b8b765ad88d71ebb86d907ffa496989633ef5e
gravell <gravell.gravell@wp.pl>
2019-01-02 21:44:27 +0000

    Added plugin manager and plugins installation to setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo Starting script...
+#It assumes that you have git, tmux and vim installed
+echo Creating symbolic links...
 SCRIPT_FULL_PATH=$(realpath $0)
 SCRIPT_DIR_PATH=$(dirname $SCRIPT_FULL_PATH)
-echo $SCRIPT_FULL_PATH
-echo $SCRIPT_DIR_PATH
 ln -s $SCRIPT_DIR_PATH/.vimrc ~/.vimrc
 ln -s $SCRIPT_DIR_PATH/.tmux.conf ~/.tmux.conf
-echo Script finished!
+echo Cloning Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo Installing vim plugins
+vim +PluginInstall +qall
+echo Done!
