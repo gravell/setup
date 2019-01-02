84fc84b5479cad4f8fd852f7c91d2e93c42ce719
gravell <gravell.gravell@wp.pl>
2019-01-02 21:29:53 +0000

    Added creating link to .vimrc and .tmux.conf files

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
 echo Starting script...
-#do stuff here
+SCRIPT_FULL_PATH=$(realpath $0)
+SCRIPT_DIR_PATH=$(dirname $SCRIPT_FULL_PATH)
+echo $SCRIPT_FULL_PATH
+echo $SCRIPT_DIR_PATH
+ln -s $SCRIPT_DIR_PATH/.vimrc ~/.vimrc
+ln -s $SCRIPT_DIR_PATH/.tmux.conf ~/.tmux.conf
 echo Script finished!
