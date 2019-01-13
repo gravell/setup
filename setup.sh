9a163be1c75633bd36e941e6615a297120c30fba
gravell <gravell.gravell@wp.pl>
2019-01-13 18:39:35 +0000

    Changed setup script messages. Added creating symbolic link for .bashrc

--- setup.sh
+++ setup.sh
@@ -6,8 +6,10 @@ SCRIPT_FULL_PATH=$(realpath $0)
 SCRIPT_DIR_PATH=$(dirname $SCRIPT_FULL_PATH)
 ln -s $SCRIPT_DIR_PATH/.vimrc ~/.vimrc
 ln -s $SCRIPT_DIR_PATH/.tmux.conf ~/.tmux.conf
-echo Cloning Vundle
+ln -s $SCRIPT_DIR_PATH/.bashrc ~/.bashrc
+echo Cloning Vundle...
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo Installing vim plugins
+echo Installing vim plugins...
 vim +PluginInstall +qall
+echo Setting colour scheme...
 echo Done!
